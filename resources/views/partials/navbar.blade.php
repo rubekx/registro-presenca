cded9755befeac926f12207cb5d0eea95472e030
rubekx <rubekx@gmail.com>
2021-12-03 08:44:49 -0300

    add redirect on click

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
 <div class="dted-header">
     <img src="img/svg/header/dted-logo.svg" class="dted-header-logo" alt="dted-logo">
-    <button type='button' class="dted-header-button">Voltar à página inicial</button>
+    <button type='button' onclick="location.href='/'" class="dted-header-button">Voltar à página inicial</button>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1 class="dted-home-h1">Registro de presença</h1>
     <h2 class="dted-home-h2">Registre aqui sua presença em eventos da DTED</h2>
-    <button class="dted-home-button">Entre para procurar seu evento</button>
+    <button onclick="location.href='/login'" class="dted-home-button">Entre para procurar seu evento</button>
     <img src="img/svg/home/people.svg" class="dted-home-people" alt="dted-logo">
 @endsection
