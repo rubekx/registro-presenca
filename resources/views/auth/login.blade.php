a5e40d08b17d53c21cb1958a4de4aed45261e27c
rubekx <rubekx@gmail.com>
2021-12-10 17:24:15 -0300

    fix css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2 ">
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default dted-search-box">
                     <div class="panel-heading">
                         <div class="dted-search-h1">Busque o seu evento</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
                 @include('partials.messages')
                 @if (!$firstSearch)
                     <div class="panel panel-default">
